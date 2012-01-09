2bab61f5148d2bb4b27c03ff01c8719e03a8f770
Jan Deinhard <jdeinhard@Jans-MacBook-Pro.local>
2012-01-09 16:14:41 +0100

    added mp3 frame files

--- server.js
+++ server.js
@@ -7,15 +7,48 @@ app.use(express.static(__dirname + '/public'));
 app.listen(8080);
 var wss = new WebSocketServer({server: app, path: '/data'});
 
+var MAX_BUFFER = 32 * 1024;
+
 wss.on('connection', function(ws) {
   ws.on('message', function(message) {
     ws.send('pong');
   });
-  fs.readFile(__dirname + '/test.mp3', function (error, data) {
+
+  var fileCount = 243;
+  var currentFile = 0;
+
+  var buffer = new Buffer(MAX_BUFFER);
+  var bufferSize = 0;
+
+  function fileHandler(error, data) {
     if (error) {
       console.log('failed to load mp3 file');
     } else {
-      ws.send(data, {binary: true});
+      
+      console.log('Buffering ' + data.length + ' bytes MP3 data.');
+      
+      if (bufferSize + data.length > MAX_BUFFER) {
+        console.log('Sending ' + bufferSize + ' bytes MP3 data.');
+        ws.send(buffer.slice(0, bufferSize), {binary: true});
+        bufferSize = 0;
+      }
+
+      data.copy(buffer, bufferSize, 0);
+      bufferSize += data.length;
+
+      currentFile += 1;
+      if (currentFile < fileCount) {
+        fs.readFile(__dirname + '/frames/frame' + currentFile + '.mp3', fileHandler);
+      } else {
+        // Out of files! Check if we have some data left in our buffer!
+        if (bufferSize > 0) {
+          console.log('Sending ' + bufferSize + ' bytes MP3 data.');
+          ws.send(buffer.slice(0, bufferSize), {binary: true});
+          bufferSize = 0;          
+        }        
+      }
     }
-  });
+  }
+
+  fs.readFile(__dirname + '/frames/frame' + currentFile + '.mp3', fileHandler);
 });
